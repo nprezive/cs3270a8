d1af1d03c49edaca8a11e8938491cfec6b616b1c
Noah Preszler <npreszler@gmail.com>
2018-03-18 22:43:55 -0600

    Added token.

---
+++ app/src/main/java/com/example/nprezive/cs3270a8/Authorization.java
@@ -0,0 +1,9 @@
+package com.example.nprezive.cs3270a8;
+
+/**
+ * Created by nprezive on 3/18/18.
+ */
+
+public class Authorization {
+    public static String AUTH_TOKEN = "14~ztoC2jn9NiezbWvQDPAd1sM0erZQ44JUQjpi0cJl9ziQnx3ruJzrH3kMDFgoxr2L";
+}
